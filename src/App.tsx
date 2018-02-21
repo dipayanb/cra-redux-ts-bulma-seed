bec688cb0605d094edb45934b123d6396023c9eb
Dipayan Bhowmick <dipayan.bhowmick@gmail.com>
2018-02-21 11:29:22 +0530

    Added folder sturcture and Auxilary HOC

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
 import * as React from 'react';
-import * as styles from './App.scss';
+import * as localStyles from './App.scss';
+import * as bulma from './themed-bulma.scss';
 const logo = require('./logo.svg');
+import Aux from 'hocs/Aux';
+
+const styles = { ...bulma, ...localStyles };
 
 class App extends React.Component {
   render() {
+    const buttonClass = [styles.button, styles.isDanger, styles.isLarge].join(
+      ' '
+    );
     return (
-      <div className={styles.app}>
-        <header className={styles.appHeader}>
-          <img src={logo} className={styles.appLogo} alt="logo" />
-          <h1 className={styles.appTitle}>Welcome to React</h1>
-        </header>
-        <p className={styles.appIntro}>
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <Aux>
+        <div className={styles.app}>
+          <header className={styles.appHeader}>
+            <img src={logo} className={styles.appLogo} alt="logo" />
+            <h1 className={styles.appTitle}>Welcome to React</h1>
+          </header>
+          <p className={styles.appIntro}>
+            To get started, edit <code>src/App.tsx</code> and save to reload.
+          </p>
+        </div>
+        <div className={styles.container}>
+          <button className={buttonClass}>Add Me</button>
+        </div>
+      </Aux>
     );
   }
 }
---
+++ src/hocs/Aux.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const Aux: React.SFC<{}> = props => {
+  return <>{props.children}</>;
+};
+
+export default Aux;
